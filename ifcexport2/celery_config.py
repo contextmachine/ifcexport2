984195596e2b7b77fd0c77a8929d38ca4c4f77ec
sth-v <sthv.developer@gmail.com>
2025-01-31 01:22:36 +0300

    1. Fix celery memory usage

--- ifcexport2/celery_config.py
+++ ifcexport2/celery_config.py
@@ -6,10 +6,11 @@ celery_app = Celery(
     "worker",
 
 )
+
 celery_app.conf.broker_url = REDIS_URL
 celery_app.conf.result_backend =REDIS_URL
+
 celery_app.conf.update(
-    result_expires=3600*6,
-    worker_max_memory_per_child=os.getenv("WORKER_MAX_MEMORY_PER_CHILD", 5242880 # 5 GB
-    )
+    result_expires=3600*6
+
 )
